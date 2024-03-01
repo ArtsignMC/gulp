95213eab9b8b623fb378105464e59fdcc46dc3ab
BuildTools <unconfigured@null.spigotmc.org>
2024-03-02 09:01:54 +1300

    Base Plugin

---
+++ src/main/java/dev/gulp/gulp/Gulp.java
@@ -0,0 +1,17 @@
+package dev.gulp.gulp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gulp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
