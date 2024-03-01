2c63d1bbb371484d99c641cb3d84f6e6fc84d67d
BuildTools <unconfigured@null.spigotmc.org>
2024-03-02 09:14:54 +1300

    Change VersionCommand.java

--- src/main/java/dev/gulp/gulp/commands/VersionCommand.java
+++ src/main/java/dev/gulp/gulp/commands/VersionCommand.java
@@ -1,4 +1,23 @@
 package dev.gulp.gulp.commands;
 
-public class VersionCommand {
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class VersionCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        //Instance of pattern match to see if a Player ran the command
+        if (sender instanceof Player){
+             Player p = (Player) sender;
+            // Send The Player The Current Plugin Version
+            p.sendMessage(ChatColor.GRAY + "GULP version " + ChatColor.GOLD + "1.0-SNAPSHOT");
+        }
+
+        return true;
+    }
 }
