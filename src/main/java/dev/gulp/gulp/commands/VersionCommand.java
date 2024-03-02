81c590acaad1a918d0914e15e0c1c9997d16a861
Fub <tabarafabian@proton.me>
2024-03-02 08:09:48 +0200

    VersionCommand

--- src/main/java/dev/gulp/gulp/commands/VersionCommand.java
+++ src/main/java/dev/gulp/gulp/commands/VersionCommand.java
@@ -1,4 +1,34 @@
 package dev.gulp.gulp.commands;
 
-public class VersionCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class VersionCommand implements CommandExecutor {
+
+    /* Fub
+        Gotta make some self-centering method here later than modifying the message with spaces manually.
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player){
+            if (command.getName().equalsIgnoreCase("version")) {
+                sender.sendMessage(
+                        "§8§m                                                  ",
+                        " ",
+                        "    §8[§6§lG.U.L.P§r§8]",
+                        "    §7Plugin version: §e§lv1.0",
+                        "    §7Support server: link",
+                        " ",
+                        "§8§m                                                  ");
+                return true;
+            }
+        }
+        else {
+            sender.sendMessage("Only players can use this command.");
+            return false;
+        }
+        return false;
+    }
 }
